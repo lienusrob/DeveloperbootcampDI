daed8304bb99168f57884e949b5138f1fc5e6ea0
lienusrob <lienus.rob@hotmail.de>
2020-12-05 18:42:01 +0200

    final

--- Hackathon/code2.py
+++ Hackathon/code2.py
@@ -1,121 +1,148 @@
-import numpy as np 
-import pygame 
-import math 
-ROWS = 6 
-COLUMNS = 7 
+import numpy as np
+import pygame
+import math
+ROWS = 6
+COLUMNS = 7
 
-board=np.zeros((ROWS, COLUMNS))
+board = np.zeros((ROWS, COLUMNS))
 
-gameover = False 
-turn = ROWS * COLUMNS 
+gameover = False
+turn = 0
 
-SLOT  =70
+SLOT = 70
 width = COLUMNS * SLOT
 height = (ROWS+1)*SLOT
 size = (width, height)
 
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-GRAY = (128,128,128)
-WHITE = ((255,255,255))
-OFFSET = 70 
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (128, 128, 128)
+WHITE = ((255, 255, 255))
+OFFSET = 70
 
 RADIUS = int(SLOT/2-5)
-
-def draw(board):
-	pygame.draw.rect(window, WHITE, (0,0,width, SLOT))
-	for c in range(COLUMNS): 
-		for r in range (ROWS): 
-			rect= (c*SLOT, r*SLOT*OFFSET, SLOT,SLOT)
-			c1= (int(c*SLOT+SLOT/2),int(r*SLOT+OFFSET+SLOT/2))
-			pygame.draw.rect(window, BLUE, rect)
-			pygame.draw.circle(window, WHITE, c1, RADIUS)
-	for c in range(COLUMNS):
-		for r in range(ROWS):
-			c2 = (int(c*SLOT+SLOT/2), height-int(r*SLOT+SLOT/2))
-			if board[r][c] == 1: 
-				pygame.draw.circle(window, RED,c2, RADIUS)
-			elif board[r][c] == 2: 
-				pygame.draw.circle(window, GREEN,c2, RADIUS)
-	pygame.display.update()
-
-
-def valid(board, cal): 
-	return board[ROWS-1][col] == 0 
-
-def drop(board, col, piece):
-	for r in range(ROWS):
-		if board[r][col]== 0:
-			row = r 
-			break 
-	board[row][col] = piece
-def win(board, piece):
-	#horizontal
-	for c in range(COLUMNS-3):
-			for r in range(ROWS):
-				if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board [r][c+3]== piece: 
-					return True 
-	#vertical
-	for c in range(COLUMNS):
-			for r in range(ROWS-3):
-				if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board [r+3][c]== piece: 
-					return True 
-	#diagonal
-	for c in range(COLUMNS-3):
-			for r in range(ROWS-3):
-				if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board [r+3][c+3]== piece: 
-					return True 
-	#diagonal.1
-	for c in range(COLUMNS-3):
-			for r in range(3, ROWS-3):
-				if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board [r-3][c+3]== piece: 
-					return True 
-print(board)
 pygame.init()
 
+
+def draw(board):
+
+    for c in range(COLUMNS):
+        for r in range(ROWS):
+            c2 = (int(c*SLOT+SLOT/2), height-int(r*SLOT+SLOT/2))
+            if board[r][c] == 1:
+                pygame.draw.circle(window, RED, c2, RADIUS)
+            elif board[r][c] == 2:
+                pygame.draw.circle(window, GREEN, c2, RADIUS)
+            else:
+                pygame.draw.circle(window, WHITE, c2, RADIUS)
+
+
+def valid(board, col):
+    return board[ROWS-1][col] == 0
+
+
+def drop(board, col, piece):
+
+    for r in range(ROWS):
+        if board[r][col] == 0:
+            board[r][col] = piece
+            return
+
+
+def win(board, piece):
+    # horizontal
+    for c in range(COLUMNS-3):
+        for r in range(ROWS):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+    # vertical
+    for c in range(COLUMNS):
+        for r in range(ROWS-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+    # diagonal
+    for c in range(COLUMNS-3):
+        for r in range(ROWS-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+    # diagonal.1
+    for c in range(COLUMNS-3):
+        for r in range(3, ROWS-3):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+
+
+print(board)
+
+
+display_winner = False
+winner = None
+
+# Font to display winner
+font = pygame.font.Font("Pixeboy.ttf", 32)
+
 window = pygame.display.set_mode(size)
-pygame.display.set_caption("connec 4")
-draw(board)
+pygame.display.set_caption("connect 4")
+
+# draw(board)
 while not gameover:
-	for event in pygame.event.get():
-		if event.type== pygame.QUIT:
-			pygame.quit()
-		
-		if event.type == pygame.MOUSEMOTION:
-			pygame.draw.rect(window, GRAY, (0,0, width, SLOT))
-			posx = event.pos[0]
-			if turn %2 ==0: #plyer one turn
-				pygame.draw.circle(window,RED,(posx, int(SLOT/2)),RADIUS)
-			else: 
-				pygame.draw.circle(window, GREEN,(posx, int(SLOT/2)),RADIUS)
-		
-		pygame.display.update 
-	
 
-		if event.type == pygame.MOUSEBUTTONDOWN:	
-			if turn %2 == 0: 
-				posx = event.pos[0]
-				col = math.floor(posx/SLOT)
-				if valid(board,col): 
-							drop(board,col,1)
-				if win(board,1):
-								print("player 1 won")
-								gameover = True 
+    # Move events to the top of the game loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameover = True
 
-		if event.type == pygame.MOUSEBUTTONDOWN:	
-			if turn %2 == 1:
-				posx = event.pos[0]
-				col = math.floor(posx/SLOT)
-				if valid(board,col):
-							drop(board,col,2)
-				if win(board, 2):
-								print("player 2 won")
-								gameover = True 
+        #  need to check if the game is still going on by "not display_winner",
+        # This is just to disable user input if we have a winner.
+        if event.type == pygame.MOUSEBUTTONDOWN and not display_winner:
+            posx = event.pos[0]
+            col = math.floor(posx/SLOT)
+            if valid(board, col):
 
-			turn += 1 
+                drop(board, col, (turn % 2)+1)
+                # Move is played and then change the turn.
+                if win(board, (turn % 2)+1):
+                    print(f"player {(turn%2) + 1} won")
+                    # If we have a winner the  variables are set
+                    display_winner = True
+                    winner = (turn % 2)+1
+                turn = 1 if turn == 0 else 0
 
-		
-			print(np.flip(board,0))
-	draw(board)
-	
+            print(np.flip(board, 0))
+
+
+    pygame.draw.rect(window, (0, 0, 0), (0, 0, width, height))
+  
+    pygame.draw.rect(window, WHITE, (0, 0, width, SLOT))
+
+    draw(board)
+
+    # If we have a winner, 
+    if display_winner:
+        text = font.render(f'Player {winner} won!', True, BLUE)
+        textRect = text.get_rect()
+        textRect.center = (width // 2, SLOT // 2)
+        window.blit(text, textRect)
+        pygame.display.update()
+        continue
+
+
+ 
+    mouse_position = pygame.mouse.get_pos()
+    # Get x, y position
+
+
+    posx = SLOT//2 + SLOT * (mouse_position[0] // SLOT)
+    # y is  SLOT // 2
+    posy = SLOT//2
+
+    # Display the preview
+    if turn % 2 == 0:
+        pygame.draw.circle(window, RED, (posx, posy), RADIUS)
+    else:
+        pygame.draw.circle(window, GREEN, (posx, posy), RADIUS)
+
+    pygame.display.update()
+
+pygame.quit()
